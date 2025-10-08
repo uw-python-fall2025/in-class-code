0a0964ac01e3f1290da5173f0a828d9180173d12
Adrienne Slaughter <ahslaughter@northeastern.edu>
2025-10-08 16:28:19 -0700

    :lipstick: Make the linter happy

--- Mod2_DataStructures/linked_list.py
+++ Mod2_DataStructures/linked_list.py
@@ -1,13 +1,14 @@
-
 def create_ll_node(data):
-    return { 'data': data,
-             'next': None }
+    return {'data': data,
+            'next': None}
+
 
 def create_ll():
-    return { 'head': None }
+    return {'head': None}
+
 
 def insert(ll, ll_node):
-    if ll['head'] is None: ## Nothing in the list
+    if ll['head'] is None:  ## Nothing in the list
         ll['head'] = ll_node
         return
     node = ll['head']
@@ -19,9 +20,11 @@ def insert(ll, ll_node):
     #     if node['next'] is None: ## It's the last node in the list
     #     node['next'] = ll_node
 
+
 def print_node(ll_node):
     data_name = ll_node['next']['data'] if ll_node['next'] else "Nothing"
     print(f'Data: {ll_node['data']}; Next: {data_name}')
+
 
 def print_list(ll_list):
     print('----')
@@ -30,6 +33,7 @@ def print_list(ll_list):
         print_node(node)
         node = node['next']
     print('----')
+
 
 my_list = create_ll()
 ace_spades = create_ll_node('Ace of Spades')
@@ -44,6 +48,3 @@ insert(my_list, two_spades)
 # print_node(six_clubs)
 
 print_list(my_list)
-
-
-
